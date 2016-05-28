6bba0253b7f23ee66251bd31a87cc25d8699c2e9
rafrex <code@rafrex.com>
2016-05-27 17:10:06 -0700

    Refactor pointer events into own handler

--- current-input.js
+++ current-input.js
@@ -60,47 +60,27 @@
     html.addEventListener('mousedown', handleMouseEnterDown, true);
   }
 
-  function pointerTypeMap(rawType) {
+  function handlePointerEvent(e) {
+    var rawType = e.pointerType;
     if (
       rawType === 'touch' ||
       rawType === 2 ||
       rawType === 'pen' ||
       rawType === 3
     ) {
-      return 'touch';
+      checkInputChange('touch');
     } else if (rawType === 'mouse' || rawType === 4) {
-      return 'mouse';
+      checkInputChange('mouse');
     }
-  }
-
-  function handlePointerDown(e) {
-    var pointerType = pointerTypeMap(e.pointerType);
-    if (pointerType === 'touch') {
-      handleTouchStart();
-    } else if (pointerType === 'mouse') {
-      handleMouseEnterDown();
-    }
-  }
-
-  function handlePointerUpCancel(e) {
-    if (pointerTypeMap(e.pointerType) === 'touch') handleTouchEndCancel();
-  }
-
-  function handlePointerEnter(e) {
-    if (pointerTypeMap(e.pointerType) === 'mouse') handleMouseEnterDown();
   }
 
   function setPointerEventListeners() {
     if (pointerEvents === 'PointerEvent') {
-      html.addEventListener('pointerdown', handlePointerDown, true);
-      html.addEventListener('pointerup', handlePointerUpCancel, true);
-      html.addEventListener('pointercancel', handlePointerUpCancel, true);
-      html.addEventListener('pointerenter', handlePointerEnter, true);
+      html.addEventListener('pointerdown', handlePointerEvent, true);
+      html.addEventListener('pointerenter', handlePointerEvent, true);
     } else if (pointerEvents === 'MSPointerEvent') {
-      html.addEventListener('MSPointerDown', handlePointerDown, true);
-      html.addEventListener('MSPointerUp', handlePointerUpCancel, true);
-      html.addEventListener('MSPointerCancel', handlePointerUpCancel, true);
-      html.addEventListener('MSPointerEnter', handlePointerEnter, true);
+      html.addEventListener('MSPointerDown', handlePointerEvent, true);
+      html.addEventListener('MSPointerEnter', handlePointerEvent, true);
     }
   }
 
