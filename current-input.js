d84b5c6d61842e43a9efb2281e6a7579fc861911
rafrex <code@rafrex.com>
2016-05-30 23:14:39 -0700

    Clean up code and add notes

--- current-input.js
+++ current-input.js
@@ -3,7 +3,7 @@
 
   var body = document.querySelector('body');
   var html = document.querySelector('html');
-  var currentInput = '';
+  var currentInput = 'none';
   var touchState = false;
   var touchTime = new Date();
 
@@ -19,16 +19,37 @@
     if (window.navigator.msMaxTouchPoints > 0) return 'msMaxTouchPoints';
   }());
 
-  function setDataAttribute(input) {
-    // body.dataset.currentinput = input;
 
-    if (currentInput !== '') body.classList.remove(currentInput);
-    body.classList.add(input);
+  // set up based on device type
+  if (touchEventsApi && (anyHover || anyPointerFine)) {
+    // hybrid device with touch events api
+    setTouchMouseEventListeners();
+    setInitialCurrentInput();
+  } else if (touchEventsApi || (pointerEvents && maxTouchPoints &&
+    (anyHover === false && anyPointerFine === false))) {
+    // required for active state to be enabled when touching the screen
+    addBlankTouchListenerToBody();
+    // touch only device, don't set listeners
+    setCurrentInputClass('touch');
+  } else if (pointerEvents && maxTouchPoints){
+    // hybrid device with pointer events api
+    setPointerEventListeners();
+    setInitialCurrentInput();
+  } else {
+    // mouse only device, don't set listeners
+    setCurrentInputClass('mouse');
+  }
+
+
+  // set current input functions
+  function setCurrentInputClass(input) {
+    body.classList.remove('current-input-' + currentInput);
+    body.classList.add('current-input-' + input);
   }
 
   function checkInputChange(input) {
     if (input !== currentInput) {
-      setDataAttribute(input);
+      setCurrentInputClass(input);
       currentInput = input;
     }
   }
@@ -42,6 +63,8 @@
     }
   }
 
+
+  // touch and mouse event functions
   function handleTouchStart() {
     touchState = true;
     checkInputChange('touch');
@@ -53,7 +76,14 @@
   }
 
   function handleMouseMoveEnterDown() {
-    if (!touchState && new Date() - touchTime > 500) checkInputChange('mouse');
+    // set state syncronously, no setTimeouts
+    // currentInput !== 'mouse' is redundent with checkInputChange('mouse'),
+    // but it short circuts the if clause without calling new Date()
+    // on every mousemove event.
+    if (!touchState && currentInput !== 'mouse' &&
+      new Date() - touchTime > 500) {
+        checkInputChange('mouse');
+      }
   }
 
   function setTouchMouseEventListeners() {
@@ -63,8 +93,20 @@
     html.addEventListener('mouseenter', handleMouseMoveEnterDown, true);
     html.addEventListener('mousedown', handleMouseMoveEnterDown, true);
     html.addEventListener('mousemove', handleMouseMoveEnterDown, true);
+    // have to use mousemove listener because on hybrid chrome
+    // touch events cause the mouse to disappear (good), and it doesn't
+    // reappear until it's moved (good), but it only fires mousemove
+    // events without firing a mouseenter event, despite re-entering the screen.
+    // Note that MS Edge fires a pointerenter event when the mouse reappears,
+    // so there is no need to set a pointermove listener.
   }
 
+  function addBlankTouchListenerToBody() {
+    html.addEventListener('touchstart', function(){}, false);
+  }
+
+
+  // pointer event functions
   function handlePointerEvent(e) {
     var rawType = e.pointerType;
     if (
@@ -83,55 +125,10 @@
     if (pointerEvents === 'PointerEvent') {
       html.addEventListener('pointerdown', handlePointerEvent, true);
       html.addEventListener('pointerenter', handlePointerEvent, true);
-      // html.addEventListener('pointermove', handlePointerEvent, true);
     } else if (pointerEvents === 'MSPointerEvent') {
       html.addEventListener('MSPointerDown', handlePointerEvent, true);
       html.addEventListener('MSPointerEnter', handlePointerEvent, true);
-      // html.addEventListener('MSPointerMove', handlePointerEvent, true);
     }
-  }
-
-
-  function addBlankTouchListenerToBody() {
-    html.addEventListener('touchstart', function(){}, false);
-  }
-
-
-  if (touchEventsApi && (anyHover || anyPointerFine)) {
-    // hybrid device with touch events api
-    setTouchMouseEventListeners();
-    setInitialCurrentInput();
-    // body.dataset.test = 'hybrid';
-    body.classList.add('hybrid');
-
-  } else if (pointerEvents && maxTouchPoints &&
-    (anyHover === false && anyPointerFine === false)) {
-    // touch only device, don't set listeners
-    addBlankTouchListenerToBody();
-    setDataAttribute('touch');
-    // body.dataset.test = 'touch-only';
-    body.classList.add('touch-only');
-
-  } else if (pointerEvents && maxTouchPoints){
-    setPointerEventListeners();
-    setInitialCurrentInput();
-    // body.dataset.test = 'hybrid';
-    body.classList.add('hybrid');
-
-  } else if (touchEventsApi) {
-    // touch only device, don't set listeners
-    setDataAttribute('touch');
-    // required for active state to be enabled when touching the screen
-    addBlankTouchListenerToBody();
-    // body.dataset.test = 'touch-only';
-    body.classList.add('touch-only');
-
-  } else {
-    // mouse only device, don't set listeners
-    setDataAttribute('mouse');
-    // body.dataset.test = 'mouse-only';
-    body.classList.add('mouse-only');
-
   }
 
 }());
