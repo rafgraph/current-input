1983d84c7934f0adc546065b837a3de222d13a76
rafrex <code@rafrex.com>
2016-07-23 19:21:08 -0700

    Add comments to index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ function setupCurrentInput() {
   const body = document.querySelector('body');
   let currentInput = undefined;
 
+  // set current input class on body
   function updateCurrentInput(input) {
     if (input !== currentInput) {
       body.classList.remove(`current-input-${currentInput}`);
@@ -13,6 +14,7 @@ function setupCurrentInput() {
     }
   }
 
+  // setup based on device type
   if (detectIt.deviceType === 'mouseOnly') updateCurrentInput('mouse');
   else if (detectIt.deviceType === 'touchOnly') {
     updateCurrentInput('touch');
@@ -23,17 +25,30 @@ function setupCurrentInput() {
     addListener(window,
       {
         'touchstart passive capture': () => { updateCurrentInput('touch'); },
+
+        /**
+         * Have to use mousemove listener because on hybrid chrome
+         * touch events cause the mouse to disappear (good), and it doesn't
+         * reappear until it's moved (good), but it only fires mousemove
+         * events without firing a mouseenter event, despite re-entering the screen.
+         * Note that MS Edge fires a pointerenter event when the mouse reappears,
+         * so there is no need to set a pointermove listener.
+         */
         'mousedown mouseenter mousemove passive capture': () => { updateCurrentInput('mouse'); },
       },
       { pointermove: false }
     );
+
+    // set initial current input state based on if the primary input can hover
     /* eslint no-unused-expressions: ["error", { "allowTernary": true }] */
     detectIt.primaryHover === 'hover' ? updateCurrentInput('mouse') : updateCurrentInput('touch');
   }
 
+  // remove event listener set by launcher (if no event listener was set this does nothing)
   document.removeEventListener('DOMContentLoaded', setupCurrentInput);
 }
 
+// launcher - calls setupCurrentInput() only after have access to the body
 (() => {
   const body = document.querySelector('body');
   if (body) setupCurrentInput();
